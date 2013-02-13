703d785eb3c601f42bd6b9193dd5023a9163b571
Karsten Gebbert <karsten@null2.net>
2013-02-13 15:28:36 +0100

    unset the default hl-line face
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-theme.el
+++ krgn-theme.el
@@ -1,5 +1,8 @@
 (load-theme 'wombat)
 
+(custom-set-faces
+ '(hl-line ((t (:background "grey12")))))
+
 ;; turn off clutter
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
