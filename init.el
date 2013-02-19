f4794e11888d868474f1c7fe403bf339bc4ba67d
Karsten Gebbert <karsten@null2.net>
2013-02-19 11:04:50 +0100

    no default config suff
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- init.el
+++ init.el
@@ -23,16 +23,3 @@
 (require 'krgn-android)
 (require 'krgn-general)
 (require 'krgn-bindings)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("944f3086f68cc5ea9dfbdc9e5846ad91667af9472b3d0e1e35a9633dcab984d5" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(hl-line ((t (:background "grey12")))))
