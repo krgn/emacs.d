7a2724dd840a44507315f952349c57f52879e313
Karsten Gebbert <karsten@null2.net>
2013-02-23 23:53:31 +0100

    fix erc
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-erc.el
+++ krgn-erc.el
@@ -1,9 +1,8 @@
 (require 'erc)
 
-(erc :server "irc.freenode.net" :port "6667" :nick "krgn"))
-
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
-
+(setq erc-nick "krgn")
+(setq erc-nick-uniquifier "$")
 (setq erc-autojoin-channels-alist '(("freenode.net" "#chicken" "#couchdb")))
 
 (provide 'krgn-erc)
