79abf21b144aeb0cd544aa9968d7233ae4c63e8b
Karsten Gebbert <karsten@null2.net>
2013-03-07 12:08:15 +0100

    include a file for improved intentation of ruby code
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-rails.el
+++ krgn-rails.el
@@ -9,6 +9,11 @@
 (setq haml-backspace-backdents-nesting nil)
 (setq haml-indent-offset 2)
 
+;;(setq ruby-deep-indent-paren nil)
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+(eval-after-load "ruby-mode" '(require 'ruby-mode-indent-fix))
+
 (add-to-list 'auto-mode-alist '("\\.haml\\'" . haml-mode))
 
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode)) 
