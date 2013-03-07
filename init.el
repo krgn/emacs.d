5b814aeeb57aca4fde1f5d022619f7e1c1b4d93f
Karsten Gebbert <karsten@null2.net>
2013-03-07 12:07:03 +0100

    youngfrog's advice on how to find var names in customize dialogs
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- init.el
+++ init.el
@@ -3,6 +3,11 @@
 ;; in large parts copied from all over the place,
 ;; but in particular from http://github/bodil/emacs.d :)
 
+;; FIND VAR NAMES:
+;; <YoungFrog> krgn: if you hit RET (or clic) on "State" and use ":" (show saved
+;;             lisp expr), you'll get the exact variable name on the left and the
+;;             value on the right (in the box)  [12:03]
+
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
 (add-to-list 'load-path dotfiles-dir)
