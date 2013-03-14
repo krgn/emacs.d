1d399dc594be961b7d211f1e256325313666f229
Karsten Gebbert <karsten@null2.net>
2013-03-14 11:22:34 +0100

    Chicken: better indentation
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-scheme.el
+++ krgn-scheme.el
@@ -3,4 +3,7 @@
 (setq scheme-program-name "csi -:c")
 (setq scheme-tags-location "~/.emacs.d/scheme-tags")
 
+;; indent handle-exceptions like defines
+(put 'handle-exceptions 'scheme-indent-function 'defun)
+
 (provide 'krgn-scheme)
