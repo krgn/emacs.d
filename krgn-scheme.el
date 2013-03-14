0fdfca63a45651ef292186e5e593d655420ef685
Karsten Gebbert <karsten@null2.net>
2013-03-14 21:46:00 +0100

    Scheme: indentation
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-scheme.el
+++ krgn-scheme.el
@@ -3,8 +3,15 @@
 (setq scheme-program-name "csi -:c")
 (setq scheme-tags-location "~/.emacs.d/scheme-tags")
 
-;; indent just like define
-(put 'handle-exceptions 'scheme-indent-function 'defun)
-(put 'make-request 'scheme-indent-function 'defun)
+(defun scheme-module-indent (state indent-point normal-indent) 0)
+(put 'module 'scheme-indent-function 'scheme-module-indent)
+(put 'handle-exceptions 'scheme-indent-function 1)
+(put 'make-request 'scheme-indent-function 1)
+(put 'and-let* 'scheme-indent-function 1)
+(put 'parameterize 'scheme-indent-function 1)
+(put 'when 'scheme-indent-function 1)
+(put 'unless 'scheme-indent-function 1)
+(put 'match 'scheme-indent-function 1)
+(put 'let-values 'scheme-indent-function 1)
 
 (provide 'krgn-scheme)
