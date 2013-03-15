4834395915119218ab07416af227db8d58cc27f3
Karsten Gebbert <karsten@null2.net>
2013-03-15 12:27:50 +0100

    use C-x k in emacsclients to close
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-general.el
+++ krgn-general.el
@@ -69,4 +69,12 @@
 
 (add-hook 'fundamental-mode-hook (highlight-todo))
 
+
+(add-hook 'server-switch-hook
+          (lambda ()
+            (when (current-local-map)
+              (use-local-map (copy-keymap (current-local-map))))
+            (when server-buffer-clients
+              (local-set-key (kbd "C-x k") 'server-edit))))
+
 (provide 'krgn-general)
