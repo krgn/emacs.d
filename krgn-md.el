fd56591493913d5c48ebf14ee0f66cc8e234db81
Karsten Gebbert <karsten@null2.net>
2013-04-21 22:40:47 +0200

    enable flyspell for markdown mode
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-md.el
+++ krgn-md.el
@@ -1,9 +1,13 @@
 (require 'markdown-mode)
+(require 'flyspell)
 
+;; globally set markdown-mode to handle .md text files
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
+;; enable flymake by default for all markdown buffers
+(add-hook 'markdown-mode-hook '(lambda () (flyspell-mode t)))
 
-(add-hook 'markdown-mode-hook (lambda () (highlight-todo)))
-            
+;; highlight certain keywords like TODO/FIXME etc..
+(add-hook 'markdown-mode-hook '(lambda () (highlight-todo)))
 
 (provide 'krgn-md)
