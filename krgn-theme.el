3f0e6ab68927eebc79e969a81a20e771744a3bed
Karsten Gebbert <karsten@null2.net>
2013-05-28 11:33:45 +0200

    hemisu theme for a change
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-theme.el
+++ krgn-theme.el
@@ -1,11 +1,20 @@
-(load-theme 'zenburn t)
+;; (load-theme 'zenburn t)
+;; (custom-set-faces
+;;  '(hl-line ((t (:background "grey20")))))
+
+;; (load-theme 'light-blue t)
+;; (custom-set-faces
+;;  '(hl-line ((t (:background "sky blue")))))
+
+(load-theme 'hemisu-dark t)
+(custom-set-faces
+ '(hl-line ((t (:background "grey20")))))
+
 
 ;; font
 (set-default-font "Monospace-10")
 (setq default-frame-alist '((font . "Monospace-10")))
 
-(custom-set-faces
- '(hl-line ((t (:background "grey20")))))
 
 ;; turn off clutter
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
