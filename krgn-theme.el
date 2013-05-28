8279356c0c1a0a2d6cbe45bb0b126f2b2f83eb83
Karsten Gebbert <karsten@null2.net>
2013-05-28 21:15:33 +0200

    stick with zenburm
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-theme.el
+++ krgn-theme.el
@@ -1,20 +1,10 @@
-;; (load-theme 'zenburn t)
-;; (custom-set-faces
-;;  '(hl-line ((t (:background "grey20")))))
-
-;; (load-theme 'light-blue t)
-;; (custom-set-faces
-;;  '(hl-line ((t (:background "sky blue")))))
-
-(load-theme 'hemisu-dark t)
+(load-theme 'zenburn t)
 (custom-set-faces
  '(hl-line ((t (:background "grey20")))))
-
 
 ;; font
 (set-default-font "Monospace-10")
 (setq default-frame-alist '((font . "Monospace-10")))
-
 
 ;; turn off clutter
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
