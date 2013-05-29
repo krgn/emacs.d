8937959332f39c33a4c05fcbaba0e01911069270
Karsten Gebbert <karsten@null2.net>
2013-05-29 21:06:15 +0200

    re-enable next/prev window binding for js3 mode
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-js.el
+++ krgn-js.el
@@ -5,4 +5,8 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js3-mode))
 (add-to-list 'auto-mode-alist '("\\.json\\'" . js3-mode))
 
+;; this is needed to keep my bindings intact across major modes
+(define-key js3-mode-map (kbd "C-x n") 'next-multiframe-window)
+(define-key js3-mode-map (kbd "C-x p") 'previous-multiframe-window)
+
 (provide 'krgn-js)
