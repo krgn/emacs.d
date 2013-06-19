ad3c81951b964d47e760928b2a61c9fb986f371c
Karsten Gebbert <karsten@null2.net>
2013-06-19 14:40:10 +0200

    indentation settings
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-rails.el
+++ krgn-rails.el
@@ -22,6 +22,13 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 
+(setq enh-ruby-hanging-brace-deep-indent-level 2)
+(setq enh-ruby-hanging-brace-indent-level 2)
+(setq enh-ruby-hanging-indent-level 2)
+(setq enh-ruby-hanging-paren-deep-indent-level 2)
+(setq enh-ruby-hanging-paren-indent-level 2)
+(setq enh-ruby-indent-level 2)
+
 (global-set-key (kbd "C-c h a") 'helm-rails-all)
 (global-set-key (kbd "C-c h c") 'helm-rails-controllers)
 (global-set-key (kbd "C-c h g c") 'helm-rails-grep-controllers)
@@ -39,5 +46,4 @@
 ;; (add-hook 'ruby-mode-hook 'hs-minor-mode)
 
 (provide 'krgn-rails)
-
 
