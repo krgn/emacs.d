f3631cf9224a1d4fed132f04091f85d7df756c8d
Karsten Gebbert <karsten@null2.net>
2013-06-20 14:24:58 +0200

    beautiful powerline
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-powerline.el
+++ krgn-powerline.el
@@ -1,6 +1,12 @@
 (require 'powerline)
+
+(custom-set-faces
+ '(powerline-active1 ((t (:foreground "#444" :background "OliveDrab" :box nil))))
+ '(powerline-active2 ((t (:foreground "#444" :background "GreenYellow" :box nil)))))
+
  ;; give your mode-line curves
 (setq powerline-arrow-shape 'arrow14)
 (powerline-default-theme)
+
 
 (provide 'krgn-powerline)
