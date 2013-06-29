fb89eadbfa694ccf5e169003a6703e4f269e9173
Karsten Gebbert <karsten@null2.net>
2013-06-29 23:38:53 +0200

    smartparens config
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-smartparens.el
+++ krgn-smartparens.el
@@ -1,5 +1,19 @@
-(require 'smartparens)
+(require 'smartparens-config)
 
+;; global mode 
 (smartparens-global-mode t)
+
+;; highlights matching pairs
+(show-smartparens-global-mode t)
+
+(sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
+
+;;; markdown-mode
+(sp-with-modes '(markdown-mode html-mode nxml-mode sgml-mode)
+  (sp-local-tag "<"  "<_>" "</_>" :transform 'sp-match-sgml-tags))
+
+;;; lisp modes
+(sp-with-modes sp--lisp-modes
+  (sp-local-pair "(" nil :bind "C-("))
 
 (provide 'krgn-smartparens)
