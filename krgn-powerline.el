9ac70e3c9b6857d20cc5d3fe6db97dae53a20a60
Karsten Gebbert <karsten@null2.net>
2013-06-30 16:19:48 +0200

    powerline colors like powerline default theme
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- krgn-powerline.el
+++ krgn-powerline.el
@@ -1,8 +1,8 @@
 (require 'powerline)
 
 (custom-set-faces
- '(powerline-active1 ((t (:foreground "#444" :background "OliveDrab" :box nil))))
- '(powerline-active2 ((t (:foreground "#444" :background "GreenYellow" :box nil)))))
+ '(powerline-active1 ((t (:foreground "#fff" :background "RoyalBlue" :box nil))))
+ '(powerline-active2 ((t (:foreground "#444" :background "CornFlowerBlue" :box nil)))))
 
  ;; give your mode-line curves
 (setq powerline-arrow-shape 'arrow)
