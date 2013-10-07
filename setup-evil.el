e7ec0732bd28dffece0b4980bbee14fd2c6a249d
Karsten Gebbert <karsten@null2.net>
2013-10-07 22:50:29 +0200

    breaking out functions to utilities file, evil mode file

---
+++ setup-evil.el
@@ -0,0 +1,7 @@
+(use-package evil
+  :init
+  (progn 
+    (evil-mode 1)
+    (setq evil-default-cursor t)
+    
+    ))
--- setup-general.el
+++ setup-general.el
@@ -5,10 +5,6 @@
 (require 'tramp)
 (require 'saveplace)
 (require 'ace-jump-mode)
-(require 'evil)
-
-(evil-mode 1)
-(setq evil-default-cursor t)
 
 ;; always font-lock plz
 (global-font-lock-mode t)
@@ -37,22 +33,11 @@
 (prefer-coding-system 'utf-8)
 
 ;; location for all backup files
-(setq backup-directory-alist
-      `(("." . ,(expand-file-name (concat dotfiles-dir "bak")))))
+(setq backup-directory-alist `(("." . ,(expand-file-name (concat dotfiles-dir "bak")))))
 
 ;; save last position in file
 (setq-default save-place t)
 (setq save-place-file (expand-file-name ".places" user-emacs-directory))
-
-;; ask before closing
-(defun ask-before-closing ()
-  "Ask whether or not to close, and then close if y was pressed"
-  (interactive)
-  (if (y-or-n-p (format "Are you sure you want to exit Emacs? "))
-      (if (< emacs-major-version 22)
-          (save-buffers-kill-terminal)
-        (save-buffers-kill-emacs))
-    (message "Canceled exit")))
 
 ;; "80 columns" is too little :)
 (setq fill-column 120)
@@ -62,27 +47,13 @@
 
 ;; don't break lines in an awkward way
 (set-default 'truncate-lines t)
-(setq inhibit-startup-screen nil)       ;don't show the startup message
+(setq inhibit-startup-screen t)       ;don't show the startup message
+(setq inhibit-splash-screen t)
 
 (setq browse-url-browser-function (quote browse-url-firefox))
 
-(defun highlight-todo ()
-  (lambda ()
-    (font-lock-add-keywords nil
-                            '(("\\<\\(FIXME\\|NOTE\\|TODO\\|IDEA\\|BUG\\):"
-                               1
-                               font-lock-warning-face t)))))
-
-(add-hook 'fundamental-mode-hook (highlight-todo))
-
-(add-hook 'server-switch-hook
-          (lambda ()
-            (when (current-local-map)
-              (use-local-map (copy-keymap (current-local-map))))
-            (when server-buffer-clients
-              (local-set-key (kbd "C-x C-c") 'server-edit))
-            (when server-buffer-clients
-              (local-set-key (kbd "C-x k") 'server-edit))))
+(add-hook 'fundamental-mode-hook 'highlight-todo)
+(add-hook 'server-switch-hook 'switch-server-hook)
 
 ;; set default directory
 (setq default-directory "~/")
@@ -100,31 +71,5 @@
 
 ;; show disambiguated paths to files in reverse order
 (setq uniquify-buffer-name-style 'reverse)
-
-;; inspired by M. Sveen
-(defun cleanup-buffer ()
-  (interactive)
-  (delete-trailing-whitespace)
-  (untabify (point-min) (point-max))
-  (indent-region (point-min) (point-max)))
-
-(defun create-scratch-buffer nil
-  "create a new scratch buffer to work in. (could be *scratch* - *scratchX*)"
-  (interactive)
-  (let ((n 0)
-        bufname)
-    (while (progn
-             (setq bufname (concat "*scratch"
-                                   (if (= n 0) "" (int-to-string n))
-                                   "*"))
-             (setq n (1+ n))
-             (get-buffer bufname)))
-    (switch-to-buffer (get-buffer-create bufname))
-    (emacs-lisp-mode)))
-
-;; close all buffers 
-(defadvice projectile-switch-project (before kill-buffers activate)
-  (projectile-kill-buffers))
-(ad-activate 'projectile-switch-project)
 
 (provide 'setup-general)
