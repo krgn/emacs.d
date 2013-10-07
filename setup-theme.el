7d14a4c86e7ee48fb639d7a99ebe82a386fc9ac5
Karsten Gebbert <karsten@null2.net>
2013-10-07 22:54:54 +0200

    move evil related stuff to evil config

--- setup-theme.el
+++ setup-theme.el
@@ -1,18 +1,6 @@
 ;; Zenburn theme and fixes for other modes
 (load-theme 'zenburn t)
 
-(lexical-let ((default-color (cons (face-background 'mode-line)
-                                   (face-foreground 'mode-line))))
-  (add-hook 'post-command-hook
-            (lambda ()
-              (let ((color (cond ((minibufferp) default-color)
-                                 ((evil-insert-state-p) '("#e80000" . "#ffffff"))
-                                 ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
-                                 ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
-                                 (t default-color))))
-                (set-face-background 'mode-line (car color))
-                (set-face-foreground 'mode-line (cdr color))))))
-
 (custom-set-faces
  '(hl-line 
    ((t (:background "grey20"))))
