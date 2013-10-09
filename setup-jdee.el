1913f73a86b7ae7adefb03a9039db1fe8377d4ad
Karsten Gebbert <karsten@null2.net>
2013-10-09 17:10:47 +0200

    config for jdee mode

--- setup-jdee.el
+++ setup-jdee.el
@@ -3,4 +3,14 @@
   :load-path "site-lisp/jdee-2.4.1/lisp"
   :init
   (progn
-    (autoload 'jde-mode "jde" "JDE mode" t)))
+    (autoload 'jde-mode "jde" "JDE mode" t)
+    (setq jde-bug-debugger-host-address "127.0.0.1")
+    (setq jde-bug-jre-home "/usr/lib/jvm/java-1.7.0-openjdk-amd64/")
+    (setq jde-compile-option-debug (quote ("all" (t nil nil))))
+    (setq jde-debugger (quote ("jdb")))
+    (setq jde-global-classpath (split-string (getenv "CLASSPATH") ":"))
+    (setq jde-jdk-doc-url "/usr/share/doc/openjdk-7-doc/html/api/index.html")
+    (setq jde-jdk-registry (quote (("1.7.0" . "/usr/lib/jvm/java-7-openjdk-amd64/"))))
+    (setq jde-jdk (quote ("1.7.0")))
+    (setq jde-regexp-jar-file "/usr/share/java/regexp.jar")
+    (setq jde-sourcepath (quote (".")))))
