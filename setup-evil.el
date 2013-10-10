5eb7ec96b26772d359e8aa2ba60fb48050cc8319
Karsten Gebbert <karsten@null2.net>
2013-10-10 12:15:20 +0200

    let there be a <leader>

--- setup-evil.el
+++ setup-evil.el
@@ -1,12 +1,12 @@
 (use-package evil
   :init
-  (progn 
+  (progn
     (evil-set-toggle-key "C-`")
     (global-set-key (kbd "C-z") nil)
 
     (evil-mode 1)
     (setq evil-default-cursor t)
-    
+
     ;; colorize the modeline according to current modal editing state
     (lexical-let ((default-color (cons (face-background 'mode-line)
                                        (face-foreground 'mode-line))))
@@ -18,4 +18,22 @@
                                      ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
                                      (t default-color))))
                     (set-face-background 'mode-line (car color))
-                    (set-face-foreground 'mode-line (cdr color))))))))
+                    (set-face-foreground 'mode-line (cdr color))))))
+
+    (use-package evil-leader
+      :init
+      (progn
+        (setq evil-leader/leader ",")
+        (setq evil-leader/in-all-states t)
+
+        (global-evil-leader-mode 1)
+
+        (evil-leader/set-key
+          "e" 'find-file
+          "w" 'save-buffer
+          "W" 'save-some-buffers
+          "k" 'kill-buffer
+          "K" 'kill-buffer-and-window
+          "d" 'dired-jump
+          "SPC" #'delete-trailing-whitespace
+          "m" 'compile)))))
