fc40e0a88ffde5e2e89b609881cfd7468946bc7d
Karsten Gebbert <karsten@null2.net>
2013-10-11 23:46:02 +0200

    use some of the installed direct features :)

--- config/setup-dired.el
+++ config/setup-dired.el
@@ -1,11 +1,18 @@
 (use-package dired
   :init
   (progn
+    (use-package dired+)
+    (use-package dired-details)
+    (use-package dired-details+)
+    (use-package dired-isearch)
+    (use-package dired-single)
+
     (put 'dired-find-alternate-file 'disabled nil)
     (setq dired-recursive-copies (quote always))
     (setq dired-recursive-deletes (quote top))
     (put 'dired-find-alternate-file 'disabled nil)
     (setq dired-dwim-target t)
+   
     (add-hook 'dired-mode-hook
               (lambda ()
                 (define-key dired-mode-map (kbd "<return>")
